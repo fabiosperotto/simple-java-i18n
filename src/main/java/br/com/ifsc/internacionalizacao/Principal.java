52253c22c319930aac55ed4abe3c316336475282
Fábio A. Sperotto <fabio.aiub@gmail.com>
2023-05-19 10:37:16 -0300

    feat: melhoria na selecao de idiomas no main para selecionar automaticamente pelas configs do SO

--- src/main/java/br/com/ifsc/internacionalizacao/Principal.java
+++ src/main/java/br/com/ifsc/internacionalizacao/Principal.java
@@ -18,12 +18,21 @@ public class Principal {
         String idioma;
         String pais;
  
-        if (args.length != 2) {
-            idioma = new String("pt");
-            pais = new String("BR");
-        } else {
-            idioma = new String(args[0]);
-            pais = new String(args[1]);
+        if(args.length != 2){ //executavel foi chamado sem argumentos/parametro cli
+            idioma = System.getProperty("user.language"); //idioma na config do sistema operacional
+            pais = System.getProperty("user.country"); //pais na config do sistema operacional
+        }else{ //temos argumentos por cli
+            idioma = args[0]; //idioma enviado por cli
+            pais = args[1]; //pais enviado por cli
+        }
+        
+        //avaliando se temos traducao disponivel
+        ArrayList<String> traducoesDisponiveis = new ArrayList<String>();
+        traducoesDisponiveis.add("pt");
+        traducoesDisponiveis.add("en");
+        if(!traducoesDisponiveis.contains(idioma)){ //nao existe traducao disponivel, usar default:
+            idioma = "pt";
+            pais = "BR";
         }
         
         Locale localCorrente;
